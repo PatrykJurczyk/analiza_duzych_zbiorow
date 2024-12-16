ec01aa3c68d5bd570ff3bc6ae6a89e5e9de3080c
Patryk Jurczyk <jurczykpatryk8@gmail.com>
2024-12-16 18:43:32 +0100

    Lab 5 i 6

---
+++ Lab5/lab5.py
@@ -0,0 +1,9 @@
+# Komendy
+
+# 1. docker build -t spark-3.5.3:v3 .
+# 2. docker-compose up
+# 2'. docker ps
+# 3. docker exec -it 2069962cbd74 bash
+# 4. source .venv/bin/activate
+# 5. python3 -m jupyterlab --ip 0.0.0.0 --no-browser
+# 6. deactivate
